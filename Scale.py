122337079d521b84f16f54caece39b3ffbb0cf04
NobodyPickedThis <aidan.akenson@gmail.com>
2026-05-19 13:43:41 -0700

    Initial scale generation

--- Scale.py
+++ Scale.py
@@ -1,7 +1,5 @@
 import random
 
-from Synth.lib import mtof
-
 class Scale():
 
     def __init__(self):
@@ -15,4 +13,181 @@ class Scale():
 
         self._notes = []
 
+        self._generate_scale()
+
         
+
+    def _generate_scale(self):
+
+        # Always include the 1
+        self._notes.append(0)
+
+        # Generate the scale
+        for i in range(self._scale_len - 1):
+
+            # Generate the note
+            new_note = 0
+            while new_note in self._notes:
+                new_note = random.randrange(1, 11)
+            self._notes.append(new_note)
+
+
+        # Check for "clumps" 
+        # No more than 2 notes should be sequential
+        for i in range(1, self._scale_len):
+
+            # Reset search counter
+            search_attempts = 0
+            
+            while (
+                self._notes[i] - 1 in self._notes and self._notes[i] + 1 in self._notes
+                ) or (
+                self._notes[i] - 1 in self._notes and self._notes[i] - 2 in self._notes
+                ) or (
+                self._notes[i] + 1 in self._notes and self._notes[i] + 2 in self._notes
+                ): 
+
+                # Generate a candidate note not already in the list
+                candidate = random.randrange(1, 11)
+                while candidate in self._notes:
+                    candidate = random.randrange(1, 11)
+                self._notes[i] = candidate
+
+                search_attempts += 1
+
+                # If a scale has gotten stuck, randomly change one of the values by 1 and try again
+                if search_attempts >= (11 - self._scale_len):
+                    note_mod_pos = random.randrange(1, self._scale_len)
+                    offset = random.choice([-1, 1])
+                    while (self._notes[note_mod_pos] + offset) in self._notes:
+                        note_mod_pos = random.randrange(1, self._scale_len)
+                        offset = random.choice([-1, 1])
+                    self._notes[note_mod_pos] += offset
+                    search_attempts = 0
+        
+        # Scales as-is are unlikely to lend themselves to 
+        # nice chords or comfortable melodies. We'll normalize
+        # a bit in the following ways:
+
+        # Ensure each scale has at least two perfect fifths somewhere
+        num_fifths = 0
+        for n in self._notes:
+            if (n + 7) % 12 in self._notes:
+                num_fifths += 1
+        
+        if num_fifths < 2:
+            # Always add the perfect fifth up from the root for simplicity
+            self._notes.append(7)
+            
+            # If another is required, generate a random one
+            if num_fifths < 1:
+                self._notes.sort()
+                new_base = random.choice(self._notes[1:])
+                new_note = (new_base + 7) % 12
+                while new_note in self._notes:
+                    new_base = random.choice(self._notes[1:])
+                    new_note = (new_base + 7) % 12
+                self._notes.append(new_note)
+
+        # Ensure each scale has at least three thirds somewhere
+        num_thirds = 0
+        for n in self._notes:
+            if (n + 3) % 12 in self._notes or (n + 4) % 12 in self._notes:
+                num_thirds += 1
+        
+        if num_thirds < 3:
+            # Always add either the major or minor third up from the root for simplicity
+            offset = random.choice([0,1])
+            if 8 + offset not in self._notes:
+                self._notes.append(3 + offset)
+            else:
+                self._notes.append(3) if offset else self._notes.append(9)
+            
+            # If more are required, generate random ones
+            if num_thirds < 2:
+                self._notes.sort()
+                new_third = (random.choice(self._notes[1:]) + random.choice([3, 4])) % 12
+                while new_third in self._notes:
+                    new_third = (random.choice(self._notes[1:]) + random.choice([3, 4])) % 12
+                self._notes.append(new_third)
+            if num_thirds < 1:
+                self._notes.sort()
+                new_third = (random.choice(self._notes[1:]) + random.choice([3, 4])) % 12
+                while new_third in self._notes:
+                    new_third = (random.choice(self._notes[1:]) + random.choice([3, 4])) % 12
+                self._notes.append(new_third)
+
+
+
+        # Ensure each scale has at least three sixths somewhere
+        num_sixths = 0
+        for n in self._notes:
+            if (n + 8) % 12 in self._notes or (n + 49) % 12 in self._notes:
+                num_sixths += 1
+        
+        if num_sixths < 3:
+            # Always add either the major or minor sixth up from the root for simplicity
+            offset = random.choice([0,1])
+            if 8 + offset not in self._notes:
+                self._notes.append(8 + offset)
+            else:
+                self._notes.append(8) if offset else self._notes.append(9)
+            
+            # If more are required, generate random ones
+            if num_sixths < 2:
+                self._notes.sort()
+                new_sixth = (random.choice(self._notes[1:]) + random.choice([8, 9])) % 12
+                while new_sixth in self._notes:
+                    new_sixth = (random.choice(self._notes[1:]) + random.choice([8, 9])) % 12
+                self._notes.append(new_sixth)
+            if num_sixths < 1:
+                self._notes.sort()
+                new_sixth = (random.choice(self._notes[1:]) + random.choice([8, 9])) % 12
+                while new_sixth in self._notes:
+                    new_sixth = (random.choice(self._notes[1:]) + random.choice([8, 9])) % 12
+                self._notes.append(new_sixth)
+
+
+        # These operations have a good chance of creating a scale
+        # whose root makes more sense on a note other than 0. To
+        # account for this, we find a candidate root and shift the
+        # other values in accordance.
+        likely_root = 0
+        for n in self._notes:
+            
+            # Start by making sure the note in question is the root of a valid
+            # major or minor chord, as we want our tonic to have that feature.
+            if ((((n + 3) % 12) in self._notes) or (((n + 4) % 12) in self._notes)) and (
+                ((n + 7) % 12) in self._notes):
+
+                # As a tiebreaker, we'll check if the dominant of n is in the scale and
+                # itself is the root of a valid major or minor chord.
+                # This should happen rarely enough to not need further tiebreakers, so
+                # this loop will arbitrarily select the highest value for which this is true.
+                if (((n + 7) % 12) in self._notes) and (
+                    (((n + 7 + 3) % 12) in self._notes) or (((n + 7 + 4) % 12) in self._notes)) and (
+                    ((n + 7 + 7) % 12) in self._notes):
+                    likely_root = n
+
+        # Rotate if needed
+        if likely_root != 0:
+            for i in range(self._scale_len):
+                self._notes[i] -= likely_root
+                if self._notes[i] < 0:
+                    self._notes[i] = 12 + self._notes[i]
+
+        self._notes.sort()
+        # FIXME checking sanity
+        print(self._notes)
+
+if __name__ == "__main__":
+    scale_test = Scale()
+    scale_test = Scale()
+    scale_test = Scale()
+    scale_test = Scale()
+    scale_test = Scale()
+    scale_test = Scale()
+    scale_test = Scale()
+    scale_test = Scale()
+    scale_test = Scale()
+    scale_test = Scale()
