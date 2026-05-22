d81de66f21385de41afce3333e27d674064a96b1
NobodyPickedThis <aidan.akenson@gmail.com>
2026-05-22 12:00:30 -0700

    Chord progression generator implemented. Still has lots of room for improvement.

--- Harmonizer.py
+++ Harmonizer.py
@@ -1,6 +1,7 @@
 import random
 
 import Scale
+import Chord
 
 class ChordProgression():
 
@@ -16,12 +17,18 @@ class ChordProgression():
             verse_key_pos = min(random.choice([1, 2, 3]), len(self._scale._triads) - 1)
             if self._scale._triads[verse_key_pos] != None:
                 verse_key = self._scale._triads[verse_key_pos][0]
-        print(verse_key)
         self._verse = self._generate_progression(verse_key)
 
         # Chorus is just in the key of the song
         chorus_key = self._scale._note_names[0]
         self._chorus = self._generate_progression(chorus_key)
+
+        print("\nVerse, key of ", verse_key, ": ", sep="", end=" ")
+        for v in self._verse:
+            print(v) 
+        print("\nChorus, key of ", chorus_key, ": ", sep="", end=" ")
+        for c in self._chorus:
+            print(c) 
 
     def _generate_progression(self, key):
         
@@ -41,7 +48,61 @@ class ChordProgression():
             harmonic_rhythm.append(num_chords_in_this_measure)
         random.shuffle(harmonic_rhythm[1:])
 
-        print(harmonic_rhythm)
+
+        # Generate the chords themselves
+        chords = []
+
+        # Ensure the first chord is in the key of the section
+        first_chord = self._scale._triads[0]
+        first_chord_finder = 0
+        while first_chord[0] != key:
+            first_chord = self._scale._triads[first_chord_finder]
+            first_chord_finder += 1
+        chords.append(first_chord)
+            
+
+        # Rest of the chords
+        for i in range(1, sum(harmonic_rhythm)):
+
+            # Generate a random root
+            root_degree = random.randrange(self._scale._scale_len)
+            root = self._scale._note_names[root_degree]
+
+            # Ensure the previous chord didn't also start with the same root
+            while chords[i - 1][0] == root:
+                root_degree = random.randrange(self._scale._scale_len)
+                root = self._scale._note_names[root_degree]
+
+            # Attempt to use a diatonic chord
+            new_chord = self._scale._triads[root_degree]
+            if self._scale._sevenths[root_degree] != None:
+                new_chord = self._scale._sevenths[root_degree]
+            if self._scale._ninths[root_degree] != None:
+                new_chord = self._scale._ninths[root_degree]
+
+            # Else we'll randomly generate either a major or minor chord
+            # FIXME make this behaviour more sophisticated once basic functionality is working
+            if new_chord is None:
+                new_chord = random.choice([Chord.major(root), Chord.minor(root)])
+
+            chords.append(new_chord)
+
+        progression = []
+        pos = 0
+        i = 0
+        while pos < sum(harmonic_rhythm) and i < duration:
+
+            if harmonic_rhythm[i] == 1:
+                progression.append(chords[pos])
+                pos += 1
+                i += 1
+            else:
+                progression.append(list([chords[pos], chords[pos + 1]]))
+                pos += 2
+                i += 1
+
+        return progression
+            
 
 if __name__ == "__main__":
     chord_prog = ChordProgression(Scale.Scale())
